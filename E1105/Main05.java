d934231544ebb88a0d21cf5c9a9eec37631f9603
Anqi <anqichen1028@gmail.com>
2025-11-08 01:17:45 -0500

    11/07 commit

--- E1105/Main05.java
+++ E1105/Main05.java
@@ -2,7 +2,23 @@ package E1105;
 import java.util.*;
 
 public class Main05 {
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println("get(1) = " + cache.get(1));
 
+        cache.put(3, 3);
+        System.out.println("get(2) = " + cache.get(2));
+
+        cache.put(4, 4);
+        System.out.println("get(1) = " + cache.get(1));
+        System.out.println("get(3) = " + cache.get(3));
+        System.out.println("get(4) = " + cache.get(4));
+
+        cache.put(3, 30);
+        System.out.println("get(3) = " + cache.get(3));
+    }
 }
 
 class LRUCache {
@@ -55,5 +71,43 @@ class LRUCache {
                 cache.remove(lru.key);
             }
         }
+    }
+
+    private void addToHead(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private Node removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        return node;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private Node removeTail() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    public void display() {
+        System.out.println("Cache: ");
+        Node cur = head.next;
+        while (cur != tail) {
+            System.out.println("key: " + cur.key + " value: " + cur.value);
+            cur = cur.next;
+        }
+        System.out.println();
     }
 }
